81becb24552a6dd010118cf8a601ece1bb45822b
David Mytton <david@arcjet.com>
2023-12-22 13:04:38 +0000

    OpenAI Next.js example

---
+++ examples/nextjs-14-openai/app/api/chat/route.ts
@@ -0,0 +1,71 @@
+// This example is adapted from https://sdk.vercel.ai/docs/guides/frameworks/nextjs-app
+import arcjet, { rateLimit } from "@arcjet/next";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from "openai";
+import { promptTokensEstimate } from "openai-chat-tokens";
+
+// Arcjet rate limit rule
+const aj = arcjet({
+  key: "ajkey_yourkey",
+  rules: [
+    rateLimit({
+      mode: "LIVE",
+      characteristics: ["ip.src"],
+      window: "1m",
+      max: 60,
+      timeout: "10m",
+    }),
+  ],
+});
+
+// OpenAI client
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  // Protect the route with Arcjet
+  const decision = await aj.protect(req);
+  console.log("Arcjet decision", decision.conclusion);
+
+  if (decision.reason.isRateLimit()) {
+    console.log("Request count", decision.reason.count);
+    console.log("Requests remaining", decision.reason.remaining);
+  }
+
+  // If the request is denied, return a 429
+  if (decision.isDenied()) {
+    if (decision.reason.isRateLimit()) {
+      return new Response("Too Many Requests", {
+        status: 429,
+      });
+    } else {
+      return new Response("Forbidden", {
+        status: 403,
+      });
+    }
+  }
+
+  // If the request is allowed, continue to use OpenAI
+  const { messages } = await req.json();
+
+  const estimate = promptTokensEstimate({
+    messages,
+  });
+
+  console.log("Token estimate", estimate);
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
