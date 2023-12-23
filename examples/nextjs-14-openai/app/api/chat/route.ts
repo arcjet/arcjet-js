0463995ada896555779733e2dc789144ef9b98a6
David Mytton <david@davidmytton.co.uk>
2023-12-23 07:33:49 +0000

    Update examples/nextjs-14-openai/app/api/chat/route.ts
    Co-authored-by: blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>

--- examples/nextjs-14-openai/app/api/chat/route.ts
+++ examples/nextjs-14-openai/app/api/chat/route.ts
@@ -20,7 +20,7 @@ const aj = arcjet({
 
 // OpenAI client
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env["OPENAI_API_KEY"] ?? "OPENAI_KEY_MISSING",
 });
 
 export const runtime = "edge";
