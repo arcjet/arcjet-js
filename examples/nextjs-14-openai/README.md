482a472eda9f95ece0c33cdbe870325a81ba8c2a
David Mytton <david@davidmytton.co.uk>
2024-01-02 15:55:37 +0000

    chore(examples): Added Next.js 14 OpenAI rate limit example (#88)

---
+++ examples/nextjs-14-openai/README.md
@@ -0,0 +1,41 @@
+<a href="https://arcjet.com" target="_arcjet-home">
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://arcjet.com/arcjet-logo-minimal-dark-mark-all.svg">
+    <img src="https://arcjet.com/arcjet-logo-minimal-light-mark-all.svg" alt="Arcjet Logo" height="128" width="auto">
+  </picture>
+</a>
+
+# Arcjet rate limit OpenAI chat route with Next.js 14
+
+This example shows how to implement a rate limit on a Next.js 14 API route which
+uses the OpenAI chat API.
+
+## How to use
+
+1. From the root of the project, install the SDK dependencies.
+
+   ```bash
+   npm ci
+   ```
+
+2. Enter this directory and install the example's dependencies.
+
+   ```bash
+   cd examples/nextjs-14-openai
+   npm ci
+   ```
+
+3. Add your OpenAI key to `.env.local`
+
+   ```env
+   OPENAI_API_KEY=
+   ```
+
+4. Start the dev server.
+
+   ```bash
+   npm run dev
+   ```
+
+5. Visit `http://localhost:3000`.
+6. Refresh the page to trigger the rate limit.
---
+++ examples/nextjs-14-openai/app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map((m) => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          {m.role === "user" ? (
+            <>
+              👤 <strong>User:</strong>{" "}
+            </>
+          ) : (
+            <>
+              🤖 <strong>AI:</strong>{" "}
+            </>
+          )}
+          {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl dark: border-gray-700 dark:bg-gray-800 dark:text-gray-100"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
