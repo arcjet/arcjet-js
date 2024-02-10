846da03f41b9f1f7f5da51fdd608f346086cbfd8
David Mytton <david@arcjet.com>
2024-02-10 16:56:58 +0000

    docs: Comment explaining option

--- examples/nextjs-13-pages-wrap/next.config.js
+++ examples/nextjs-13-pages-wrap/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Disabled due to errors when building. See
+  // https://nextjs.org/docs/messages/failed-loading-swc
   swcMinify: false,
 }
 
