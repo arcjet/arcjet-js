fc6c6a8611a06a1efb2d45f54bea2a3128b4cc2e
David Mytton <david@davidmytton.co.uk>
2024-03-11 21:35:53 +0000

    docs(examples): Add Node.js express server validate email example (#343)
    * docs(examples): Added Node.js express server validate email example
    
    * Fix email
    
    * Remove body parser as its built in
    
    * Add dependabot config
    
    * Update examples/nodejs-express-validate-email/index.js
    
    ---------
    
    Co-authored-by: blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>

---
+++ examples/nodejs-express-validate-email/index.js
@@ -0,0 +1,40 @@
+import arcjet, { validateEmail } from "@arcjet/node";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+
+const aj = arcjet({
+  // Get your site key from https://app.arcjet.com and set it as an environment
+  // variable rather than hard coding.
+  key: process.env.ARCJET_KEY,
+  rules: [
+    validateEmail({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      block: ["NO_MX_RECORDS"], // block email addresses with no MX records
+    }),
+  ],
+});
+
+app.post('/', async (req, res) => {
+  console.log("Email received: ", req.body.email)
+
+  const decision = await aj.protect(req, {
+    email: req.body.email,
+  });
+  console.log("Arcjet decision", decision);
+
+  if (decision.isDenied()) {
+    res.writeHead(403, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Forbidden" }));
+  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Hello World", email: req.body.email }));
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
