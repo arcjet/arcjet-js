66dad603693035d3c1b323be9e9b9163ef7afe6a
David Mytton <david@arcjet.com>
2024-03-25 11:41:26 +0000

    docs(examples): Added Auth.js 5 example app

---
+++ examples/nextjs-14-authjs-5/app/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+export { handlers } from "auth";
+import arcjet, { detectBot, slidingWindow } from "@arcjet/next";
+import { handlers } from "auth";
+import { NextRequest, NextResponse } from "next/server";
+
+const aj = arcjet({
+    key: process.env.ARCJET_KEY,
+    rules: [
+      slidingWindow({
+        mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+        interval: 60, // tracks requests across a 60 second sliding window
+        max: 10, // allow a maximum of 10 requests
+      }),
+      detectBot({
+        mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+        block: ["AUTOMATED"], // blocks all automated clients
+      }),
+    ],
+});
+
+// Protect the sensitive actions e.g. login, signup, etc with Arcjet
+const ajProtectedPOST = async (req: NextRequest) => {
+  const decision = await aj.protect(req);
+  console.log("Arcjet decision", decision);
+
+  if (decision.isDenied()) {
+    if (decision.reason.isRateLimit()) {
+      return NextResponse.json({ error: "Too Many Requests" }, { status: 429 });
+    } else {
+      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+    }
+  }
+
+  
+  return handlers.POST(req);
+};
+
+// You could also protect the GET handler, but these tend to be less sensitive
+// so it's not always necessary
+const GET = async (req: NextRequest) => {
+    return handlers.GET(req);
+}
+
+export { GET, ajProtectedPOST as POST };
