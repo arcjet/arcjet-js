b0a13a45be66044b65d1ed4295bd92ffbdf45186
David Mytton <david@davidmytton.co.uk>
2024-04-16 11:37:42 -0400

    chore(examples): Added new Next.js + React Hook Form example (#559)
    Added new Next.js + React Hook Form example. Also adds the Vercel Deploy button ref https://github.com/arcjet/arcjet/issues/1397 and closes #576.

---
+++ examples/nextjs-14-react-hook-form/config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Arcjet + Next.js + React Hook Form",
+  description: "Protecting a Next.js React Hook Form with Arcjet.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+};
