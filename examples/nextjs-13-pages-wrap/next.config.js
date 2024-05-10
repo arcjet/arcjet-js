0cafdbe5353def6e3a0e3587e1f5e4052e1c550a
blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>
2024-05-10 05:49:56 -0700

    chore(example): Remove Next 13 example (#734)
    This removes the Next.js 13 example we had because there is a high vulnerability: https://github.com/arcjet/arcjet-js/security/dependabot/9
    
    The vulnerability was fixed in 14.1.1 but Vercel doesn't backport patches to Next 13, so we should no longer be recommending anyone use vulnerable versions of Next.

--- examples/nextjs-13-pages-wrap/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Disabled due to errors when building. See
-  // https://nextjs.org/docs/messages/failed-loading-swc
-  swcMinify: false,
-}
-
-module.exports = nextConfig
