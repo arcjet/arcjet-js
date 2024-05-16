f47fefd2937cb324ebd43d5f6e8b579b13330f7a
Blaine Bublitz <blaine@arcjet.com>
2024-05-16 15:25:12 -0400

    chore(eslint-config)!: Remove nextjs linting rules & update other linting rules

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -1,11 +1,24 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "turbo", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "turbo",
+    "prettier",
+  ],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-unused-vars": "off",
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    es2022: true,
   },
   parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
+    ecmaVersion: "2022",
+    sourceType: "module",
   },
 };
