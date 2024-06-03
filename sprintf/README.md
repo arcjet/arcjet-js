a49e8536c3fd6c5ffa16ee69d3e67fa985bf3e2f
Blaine Bublitz <blaine@arcjet.com>
2024-06-03 15:08:25 -0400

    feat: Create sprintf package to replace util.format

---
+++ sprintf/README.md
@@ -0,0 +1,70 @@
+<a href="https://arcjet.com" target="_arcjet-home">
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://arcjet.com/arcjet-logo-dark-planet-arrival.svg">
+    <img src="https://arcjet.com/arcjet-logo-light-planet-arrival.svg" alt="Arcjet Logo" height="144" width="auto">
+  </picture>
+</a>
+
+# `@arcjet/sprintf`
+
+<p>
+  <a href="https://www.npmjs.com/package/@arcjet/sprintf">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="https://img.shields.io/npm/v/%40arcjet%2Fsprintf?style=flat-square&label=%E2%9C%A6Aj&labelColor=000000&color=5C5866">
+      <img alt="npm badge" src="https://img.shields.io/npm/v/%40arcjet%2Fsprintf?style=flat-square&label=%E2%9C%A6Aj&labelColor=ECE6F0&color=ECE6F0">
+    </picture>
+  </a>
+</p>
+
+[Arcjet][arcjet] platform-independent replacement for [util.format][node-util].
+
+## Installation
+
+```shell
+npm install -S @arcjet/sprintf
+```
+
+## Example
+
+```ts
+import format from "@arcjet/sprintf";
+
+format("Hello %s", "world") === "Hello world";
+format("1 %i %d", 2, 3.0) === "1 2 3";
+```
+
+## Substitutions
+
+Substitutions will be made for the following character sequences if the matching
+argument conforms to the type. For example, `"%d"` will only be replaced by a
+number, not a string or object.
+
+Object substitution supports any value that is not `undefined`.
+
+- `%d` | `%f` - Replaced if provided with a number.
+- `%i` - Replaced if provided with a number after `Math.floor` is called on it.
+- `%O` | `%o` | `%j` - Replaced if provided with any value after
+  `JSON.stringify` is called on it. Objects with circular references will be
+  replaced with `[Circular]`. Functions will be replaced with the function name
+  or `<anonymous>` if unnamed.
+- `%s` - Replaced if provided with a string.
+- `%%` - Replaced by the literal `%` character.
+
+## Implementation
+
+This implementation of this library is based on [quick-format-unescaped], which
+is licensed MIT with licenses included in our source code.
+
+The goal of this library is to be more restrictive than `quick-format-unescaped`
+while maintaining as much compatibility as possible, since [pino] uses it to
+format strings.
+
+## License
+
+Licensed under the [Apache License, Version 2.0][apache-license].
+
+[arcjet]: https://arcjet.com
+[node-util]: https://nodejs.org/docs/latest-v18.x/api/util.html#utilformatformat-args
+[quick-format-unescaped]: https://github.com/pinojs/quick-format-unescaped/blob/20ebf64c2f2e182f97923a423d468757b9a24a63/index.js
+[pino]: https://github.com/pinojs/pino
+[apache-license]: http://www.apache.org/licenses/LICENSE-2.0
---
+++ sprintf/index.ts
@@ -0,0 +1,162 @@
+function tryStringify(o: unknown) {
+  try {
+    return JSON.stringify(o);
+  } catch (e) {
+    return "[Circular]";
+  }
+}
+
+const PERCENT_CODE = 37; /* % */
+const LOWERCASE_D_CODE = 100; /* d */
+const LOWERCASE_F_CODE = 102; /* f */
+const LOWERCASE_I_CODE = 105; /* i */
+const UPPERCASE_O_CODE = 79; /* O */
+const LOWERCASE_O_CODE = 111; /* o */
+const LOWERCASE_J_CODE = 106; /* j */
+const LOWERCASE_S_CODE = 115; /* s */
+
+// Heavily based on https://github.com/pinojs/quick-format-unescaped
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2016-2019 David Mark Clements
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+export default function sprintf(str: string, ...args: unknown[]): string {
+  if (typeof str !== "string") {
+    throw new TypeError("First argument must be a string");
+  }
+
+  const argsLength = args.length;
+  if (argsLength === 0) {
+    return str;
+  }
+
+  let output = "";
+  let argIdx = 0;
+  let lastPosition = -1;
+  const strLength = str.length;
+  for (let i = 0; i < strLength; ) {
+    if (str.charCodeAt(i) === PERCENT_CODE && i + 1 < strLength) {
+      lastPosition = lastPosition > -1 ? lastPosition : 0;
+      switch (str.charCodeAt(i + 1)) {
+        case LOWERCASE_D_CODE:
+        case LOWERCASE_F_CODE: {
+          if (argIdx >= argsLength) {
+            break;
+          }
+          const arg = args[argIdx];
+          if (typeof arg !== "number") {
+            break;
+          }
+          if (lastPosition < i) {
+            output += str.slice(lastPosition, i);
+          }
+          output += arg;
+          lastPosition = i + 2;
+          i++;
+          break;
+        }
+        case LOWERCASE_I_CODE: {
+          if (argIdx >= argsLength) {
+            break;
+          }
+          const arg = args[argIdx];
+          if (typeof arg !== "number") {
+            break;
+          }
+          if (lastPosition < i) {
+            output += str.slice(lastPosition, i);
+          }
+          output += Math.floor(arg);
+          lastPosition = i + 2;
+          i++;
+          break;
+        }
+        case UPPERCASE_O_CODE:
+        case LOWERCASE_O_CODE:
+        case LOWERCASE_J_CODE: {
+          if (argIdx >= argsLength) {
+            break;
+          }
+          const arg = args[argIdx];
+          if (arg === undefined) {
+            break;
+          }
+          if (lastPosition < i) {
+            output += str.slice(lastPosition, i);
+          }
+          if (typeof arg === "string") {
+            output += `'${arg}'`;
+            lastPosition = i + 2;
+            i++;
+            break;
+          }
+          if (typeof arg === "function") {
+            output += arg.name || "<anonymous>";
+            lastPosition = i + 2;
+            i++;
+            break;
+          }
+          output += tryStringify(arg);
+          lastPosition = i + 2;
+          i++;
+          break;
+        }
+        case LOWERCASE_S_CODE: {
+          if (argIdx >= argsLength) {
+            break;
+          }
+          const arg = args[argIdx];
+          if (typeof arg !== "string") {
+            break;
+          }
+          if (lastPosition < i) {
+            output += str.slice(lastPosition, i);
+          }
+          output += arg;
+          lastPosition = i + 2;
+          i++;
+          break;
+        }
+        case PERCENT_CODE: {
+          if (lastPosition < i) {
+            output += str.slice(lastPosition, i);
+          }
+          output += "%";
+          lastPosition = i + 2;
+          i++;
+          argIdx--;
+          break;
+        }
+      }
+      ++argIdx;
+    }
+    ++i;
+  }
+  if (lastPosition === -1) {
+    return str;
+  }
+
+  if (lastPosition < strLength) {
+    output += str.slice(lastPosition);
+  }
+
+  return output;
+}
