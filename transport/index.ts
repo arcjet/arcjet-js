3faaa4d7513ec7a688250d93f3d99d3415f7e3e7
Blaine Bublitz <blaine@arcjet.com>
2024-07-26 14:35:44 -0400

    chore: Attempt to warm http2 connection upon SDK startup

--- transport/index.ts
+++ transport/index.ts
@@ -1,8 +1,21 @@
-import { createConnectTransport } from "@connectrpc/connect-node";
+import {
+  createConnectTransport,
+  Http2SessionManager,
+} from "@connectrpc/connect-node";
 
 export function createTransport(baseUrl: string) {
+  // We create our own session manager so we can attempt to pre-connect
+  const sessionManager = new Http2SessionManager(baseUrl, {
+    // AWS ALB doesn't support PING so we use a very high idle timeout
+    idleConnectionTimeoutMs: 4000 * 1000,
+  });
+
+  // We ignore the promise result because this is an optimistic pre-connect
+  sessionManager.connect();
+
   return createConnectTransport({
     baseUrl,
     httpVersion: "2",
+    sessionManager,
   });
 }
