5be2973f91584ff7f76097f7850bd8a29a2b33f9
blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>
2024-07-31 06:10:09 -0700

    Update transport/index.ts

--- transport/index.ts
+++ transport/index.ts
@@ -7,7 +7,7 @@ export function createTransport(baseUrl: string) {
   // We create our own session manager so we can attempt to pre-connect
   const sessionManager = new Http2SessionManager(baseUrl, {
     // AWS ALB doesn't support PING so we use a very high idle timeout
-    idleConnectionTimeoutMs: 4000 * 1000,
+    idleConnectionTimeoutMs: 600 * 1000,
   });
 
   // We ignore the promise result because this is an optimistic pre-connect
