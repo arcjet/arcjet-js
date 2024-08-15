67e895b3911b8195218633950f7ea085775b4063
e-moran <eoin@arcjet.com>
2024-08-15 16:56:06 +0100

    remove generated files

--- analyze/convert.d.ts
+++
@@ -1,8 +0,0 @@
-import * as core from "./wasm/arcjet_analyze_js_req.component.js";
-import { SensitiveInfoEntity } from "@arcjet/protocol";
-export declare function ConvertProtocolEntitiesToAnalyzeEntities(
-  entity: SensitiveInfoEntity<any>,
-): core.SensitiveInfoEntity;
-export declare function ConvertAnalyzeEntitiesToProtocolEntities(
-  entity: core.SensitiveInfoEntity,
-): SensitiveInfoEntity<any>;
