fdfcaf3e0242e9b4a6d9db5d93dc601f983bae3c
blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>
2024-10-01 08:31:50 -0700

    feat: Add Deno adapter (#1782)
    This adds a Deno adapter to our SDK. The implementation is very close to the Bun implementation with the platform-specific APIs swapped for Deno.
    
    The work we did to make our core platform-agnostic has really paid off.
    
    I created an example that uses `sensitiveInfo` to verify that our body reading logic works correctly. You may notice a bunch of relative `imports` in the `deno.json` file, which is a workaround for loading all arcjet-js packages from the monorepo. I believe these won't be required once we load this via the `npm:@arcjet/deno` specifier, but we won't know for certain until it is published.
    
    Closes #758

---
+++ examples/deno-sensitive-info/server.ts
@@ -0,0 +1,21 @@
+import "jsr:@std/dotenv/load";
+
+import arcjet, { sensitiveInfo, shield } from "npm:@arcjet/deno"
+
+const aj = arcjet({
+  key: Deno.env.get("ARCJET_KEY")!,
+  rules: [
+    shield({ mode: "LIVE" }),
+    sensitiveInfo({ mode: "LIVE", allow: [] })
+  ]
+})
+
+Deno.serve({ port: 3000 }, aj.handler(async (request: Request) => {
+  const decision = await aj.protect(request)
+
+  if (decision.isDenied()) {
+    return new Response("Forbidden", { status: 403 });
+  }
+
+  return new Response("Hello, world!");
+}));
