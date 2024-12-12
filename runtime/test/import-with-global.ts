4bca243c2dbc1f3956eaeeb48e1193ce41176ecd
Blaine Bublitz <blaine@arcjet.com>
2024-12-12 12:12:51 -0700

    build importer util

--- runtime/test/import-with-global.ts
+++ runtime/test/import-with-global.ts
@@ -1,30 +1,29 @@
 import { createContext, runInContext, SourceTextModule } from "node:vm";
 import { readFile } from "node:fs/promises";
 
-export async function importWithGlobal(target, global) {
-
-  async function linker(specifier) {
+export async function importWithGlobal(
+  target: string,
+  global: Record<string, unknown>,
+) {
+  const context = createContext(global);
+  const mod = new SourceTextModule(`export * from "${target}"`, { context });
+  await mod.link(async (specifier) => {
     if (specifier === target) {
       const src = await readFile(new URL(specifier, import.meta.url), "utf-8");
       return new SourceTextModule(src, { context });
     }
 
     throw new Error(`Unable to link unknown specifier: ${specifier}`);
-  }
-
-
-  const context = createContext(global);
-  const mod = new SourceTextModule(`export * from "${target}"`, { context });
-  await mod.link(linker);
+  });
 
   const isolatedModule = runInContext(`import("${target}")`, context, {
     importModuleDynamically(specifier, script, assertions) {
       if (specifier === target) {
-        return mod
+        return mod;
       }
 
       throw new Error(`Unable to import unknown specifier: ${specifier}`);
-    }
-  })
-  return isolatedModule
+    },
+  });
+  return isolatedModule;
 }
