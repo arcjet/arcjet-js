33a70a146b421e4b00692781b65e18996145e0ef
Blaine Bublitz <blaine@arcjet.com>
2025-03-12 16:44:16 -0700

    chore(examples): Add gatsby example

---
+++ examples/gatsby-rate-limit/src/api/arcjet.ts
@@ -0,0 +1,36 @@
+import arcjet, { slidingWindow } from "@arcjet/node";
+import type { GatsbyFunctionRequest, GatsbyFunctionResponse } from 'gatsby';
+
+const aj = arcjet({
+    key: process.env.ARCJET_KEY!,
+    rules: [
+        slidingWindow({
+            mode: "LIVE",
+            interval: "1m",
+            max: 1,
+        }),
+    ],
+});
+
+export default async function handler(
+    req: GatsbyFunctionRequest,
+    res: GatsbyFunctionResponse
+) {
+    const decision = await aj.protect(req);
+
+    console.log("Arcjet decision", decision);
+
+    if (decision.isErrored()) {
+        console.error("Encountered Arcjet Error", decision.reason);
+    }
+
+    if (decision.isDenied()) {
+        if (decision.reason.isRateLimit()) {
+            return res.status(429).json({ error: "Too many requests" });
+        }
+
+        return res.status(403).json({ error: "Forbidden" });
+    }
+
+    return res.json({ message: "Hello world!" });
+}
