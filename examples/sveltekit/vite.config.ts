8133d75677f73c44dc288a245942105bfd5e8237
Blaine Bublitz <blaine@arcjet.com>
2025-03-27 15:24:50 -0700

    trying to understand vercel adapter

--- examples/sveltekit/vite.config.ts
+++ examples/sveltekit/vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			"node:env": "$env/dynamic/private"
+		}
+	},
+	ssr: {
+		external: ["@arcjet/sveltekit"],
+	},
 	plugins: [sveltekit()]
 });
