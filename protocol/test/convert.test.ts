46a413abc5f1d2e74b0784c77cc2922e71fae968
Titus Wormer <titus@arcjet.com>
2025-06-23 11:39:17 +0200

    move

--- protocol/test/convert.test.ts
+++ protocol/test/convert.test.ts
@@ -811,21 +811,6 @@ test("convert", async (t) => {
     );
   });
 
-  await t.test("ArcjetRuleResult", async (t) => {
-    await t.test("ArcjetRuleResult#isDenied", () => {
-      const result = new ArcjetRuleResult({
-        conclusion: "ALLOW",
-        fingerprint: "fingerprint",
-        reason: new ArcjetReason(),
-        ruleId: "rule-id",
-        state: "RUN",
-        ttl: 0,
-      });
-
-      assert.equal(result.isDenied(), false);
-    });
-  });
-
   await t.test("ArcjetDecisionToProtocol", async (t) => {
     await t.test(
       "should create a protocol decision from an arcjet decision",
@@ -1253,93 +1238,6 @@ test("convert", async (t) => {
         }),
       );
       assert.equal(rule.rule.case, "sensitiveInfo");
-    });
-  });
-
-  await t.test("ArcjetIpDetails", async (t) => {
-    const ipDetails = new ArcjetIpDetails(
-      new IpDetails({
-        asnCountry: "a",
-        asnDomain: "b",
-        asnName: "c",
-        asnType: "d",
-        asn: "e",
-        city: "f",
-        continentName: "g",
-        continent: "h",
-        countryName: "i",
-        country: "j",
-        latitude: 40.7127,
-        longitude: 74.0059,
-        postalCode: "k",
-        region: "l",
-        service: "m",
-        timezone: "America/New_York",
-      }),
-    );
-
-    await t.test("hasASN", () => {
-      assert.equal(ipDetails.hasASN(), true);
-    });
-
-    await t.test("hasAccuracyRadius", () => {
-      assert.equal(ipDetails.hasAccuracyRadius(), true);
-    });
-
-    await t.test("hasCity", () => {
-      assert.equal(ipDetails.hasCity(), true);
-    });
-
-    await t.test("hasContintent", () => {
-      assert.equal(ipDetails.hasContintent(), true);
-    });
-
-    await t.test("hasCountry", () => {
-      assert.equal(ipDetails.hasCountry(), true);
-    });
-
-    await t.test("hasLatitude", () => {
-      assert.equal(ipDetails.hasLatitude(), true);
-    });
-
-    await t.test("hasLongitude", () => {
-      assert.equal(ipDetails.hasLongitude(), true);
-    });
-
-    await t.test("hasPostalCode", () => {
-      assert.equal(ipDetails.hasPostalCode(), true);
-    });
-
-    await t.test("hasRegion", () => {
-      assert.equal(ipDetails.hasRegion(), true);
-    });
-
-    await t.test("hasService", () => {
-      assert.equal(ipDetails.hasService(), true);
-    });
-
-    await t.test("hasTimezone", () => {
-      assert.equal(ipDetails.hasTimezone(), true);
-    });
-
-    await t.test("isHosting", () => {
-      assert.equal(ipDetails.isHosting(), false);
-    });
-
-    await t.test("isProxy", () => {
-      assert.equal(ipDetails.isProxy(), false);
-    });
-
-    await t.test("isRelay", () => {
-      assert.equal(ipDetails.isRelay(), false);
-    });
-
-    await t.test("isTor", () => {
-      assert.equal(ipDetails.isTor(), false);
-    });
-
-    await t.test("isVpn", () => {
-      assert.equal(ipDetails.isVpn(), false);
     });
   });
 });
