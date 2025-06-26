02e23806d3e82125fae0b77bb9ce61da4efadaab
Titus Wormer <titus@arcjet.com>
2025-06-26 13:42:34 +0200

    chore: fix typo in svelte example

--- examples/sveltekit/src/routes/+page.svelte
+++ examples/sveltekit/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Arcjet demo on SvelteKit</h1>
 <p>
-	This page is proected by <a href="https://docs.arcjet.com/shield" target="_blank">Shield</a>. Take
+	This page is protected by <a href="https://docs.arcjet.com/shield" target="_blank">Shield</a>. Take
 	a look at <code>/src/hooks.server.ts</code> to see how the Arcjet object is initialised.
 </p>
 <p>
