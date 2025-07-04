0c7961f635566d4bcf191d8a3c409d6b325b366d
Titus Wormer <titus@arcjet.com>
2025-07-04 17:36:08 +0200

    move characteristics in example too

--- examples/fastify-rate-limit/index.js
+++ examples/fastify-rate-limit/index.js
@@ -12,10 +12,6 @@ if (!arcjetKey) {
 }
 
 const arcjet = arcjetFastify({
-  // You can add a `characteristics` field here to track clients.
-  // The default is `["ip.src"]`.
-  // See <https://docs.arcjet.com/architecture/#fingerprinting> for more info.
-
   key: arcjetKey,
 
   rules: [
@@ -23,6 +19,10 @@ const arcjet = arcjetFastify({
     // Arcjet also supports other types (sliding window, token bucket).
     // See <https://docs.arcjet.com/rate-limiting/reference/> for more info.
     fixedWindow({
+      // You can add a `characteristics` field here to track clients.
+      // The default is `["ip.src"]`.
+      // See <https://docs.arcjet.com/architecture/#fingerprinting> for more info.
+
       max: 1, // Allow a single request (for demo purposes).
       mode: "LIVE", // Use `DRY_RUN` instead of `LIVE` to only log.
       window: "1m", // …reset after this duration.
