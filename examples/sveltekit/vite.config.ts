dcc1a48d52f4378674cd07ce194c2b56837af041
Titus Wormer <titus@arcjet.com>
2025-07-18 15:17:45 +0200

    build(examples): add strict type checking

--- examples/sveltekit/vite.config.ts
+++ examples/sveltekit/vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	// @ts-expect-error: there is some weird deep type error that says it doesn’t match.
 	plugins: [sveltekit()]
 });
