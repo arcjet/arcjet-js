9beafabc95ea6123436b575d397d61506d983677
Titus Wormer <titus@arcjet.com>
2025-07-18 11:52:18 +0200

    amend notes

--- stable-hash/test/index.test.ts
+++ stable-hash/test/index.test.ts
@@ -4,10 +4,7 @@ import test from "node:test";
 test("@arcjet/stable-hash", async function (t) {
   await t.test("should expose the public api", async function () {
     assert.deepEqual(Object.keys(await import("../index.js")).sort(), [
-      // TODO(@wooorm-arcjet): discuss different names,
-      // `hashBool`, `hashString`, `hashUint32`.
-      // For `stringSliceOrdered`, the ordering seems to be implied for stable hashing,
-      // `slice` seems to mean array`, perhaps `arrayString` or `stringArray`?
+      // TODO(@wooorm-arcjet): discuss exposing `hasher` as a named export.
       "bool",
       "hash",
       "makeHasher",
