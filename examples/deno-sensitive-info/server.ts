e20921c8a6072fca3b010088e367cc1eb0d95ad2
Titus Wormer <titus@arcjet.com>
2025-07-21 15:03:11 +0200

    make deno work?

--- examples/deno-sensitive-info/server.ts
+++ examples/deno-sensitive-info/server.ts
@@ -1,4 +1,4 @@
-/// <reference types="https://deno.land/x/pkg@1.0.0/types.d.ts" />
+/// <reference lib="deno.ns" />
 
 import "jsr:@std/dotenv/load";
 
@@ -13,7 +13,7 @@ const aj = arcjet({
 })
 
 Deno.serve({ port: 3000 }, aj.handler(async (request: Request) => {
-  const decision = await aj.protect(request)
+  const decision = await aj.protect(request, {})
 
   if (decision.isDenied()) {
     return new Response("Forbidden", { status: 403 });
