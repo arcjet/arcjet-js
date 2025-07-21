aea416a6d85a4709bee69ac27dbbaa10e8574bea
Titus <titus@arcjet.com>
2025-07-21 12:53:12 +0200

    test: clean `ip` tests (#4632)
    In the IP tests, there were functions making functions making objects making assertions.
    It was difficult to wrap my head around what was going on.
    Functions have some benefits but also makes it hard to keep track of what’s going on, creating arguably potentially smelly code.
    The alternative is to be a bit more verbose in each test case. Less functions to build things. Loops and arrays for repetition.
    I did that.
    
    Some examples of arguable smells were: 
    
    * exact same tests running many times (https://redirect.github.com/arcjet/arcjet-js/blob/03c5da93b33728a1d9b561ef72b4a7e0bf010fcc/ip/test/ipv4.test.ts#L9, note that `make` is not used in these)
    * some tests don’t exactly fit the abstractions, so then you need to attach them separately and do things manually (https://redirect.github.com/arcjet/arcjet-js/blob/03c5da93b33728a1d9b561ef72b4a7e0bf010fcc/ip/test/ipv6.test.ts#L205)
    * similarly, options need to be passed to at different abstractions, the `platform` option deals with which headers are supported but the `proxy` option which IP is supported, but then the question is, what option takes precedence at what abstraction? What is really tested?
    * a split down ipv4 and ipv6, with similar copy/pasted code
    
    That being said, I importantly did not *change* the tests. Didn’t add, move, or remove. But there is room for that.
    I do wonder whether we really need to test every potential valid/invalid IP (64 tests) at every header or other property in the request.
    More important seems a split around platforms, to test that most headers are *not* supported if a platform is set. Which is documented in the code but currently not tested.
    
    Still, for the same number of lines, I do think it’s now more obvious what is going on.
    I left the commits so work progress can be seen, but that shows a lot of the in-between-state, I’d recommend side by side.

---
+++ ip/test/ip.test.ts
@@ -0,0 +1,619 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import findIp, { parseProxy } from "../index.js";
+
+type Proxy = ReturnType<typeof parseProxy>;
+
+type Case = [
+  message: string,
+  input: string,
+  expected: string,
+  proxies?: Array<Proxy>,
+];
+
+const cases: Array<Case> = [
+  ["returns empty string if unspecified", "0.0.0.0", ""],
+  ["returns empty string if 'this network' address", "0.1.2.3", ""],
+  [
+    "returns empty string if in the shared address range",
+    "100.127.255.255",
+    "",
+  ],
+  [
+    "returns empty string if in the link local address range",
+    "169.254.255.255",
+    "",
+  ],
+  ["returns empty string if in the future protocol range", "192.0.0.1", ""],
+  [
+    "returns empty string if in the 192.0.2.x documentation range",
+    "192.0.2.1",
+    "",
+  ],
+  [
+    "returns empty string if in the 198.51.100.x documentation range",
+    "198.51.100.1",
+    "",
+  ],
+  [
+    "returns empty string if in the 203.0.113.x documentation range",
+    "203.0.113.1",
+    "",
+  ],
+  ["returns empty string if in the benchmarking range", "198.19.255.255", ""],
+  ["returns empty string if in the reserved range", "240.0.0.0", ""],
+  ["returns empty string if in the broadcast address", "255.255.255.255", ""],
+  ["returns empty string if loopback", "127.0.0.1", ""],
+  ["returns empty string if not full ip", "12.3.4", ""],
+  ["returns empty string if more than 3 digits in an octet", "1111.2.3.4", ""],
+  ["returns empty string if more than full ip", "1.2.3.4.5", ""],
+  ["returns empty string if any octet has leading 0", "1.02.3.4", ""],
+  [
+    "returns empty string if not a string",
+    // @ts-expect-error: test how runtime handles non-string input.
+    ["12", "3", "4"],
+    "",
+  ],
+  ["returns empty string if in the 10.x.x.x private range", "10.1.1.1", ""],
+  [
+    "returns empty string if in the 172.16.x.x-172.31.x.x private range",
+    "172.18.1.1",
+    "",
+  ],
+  [
+    "returns empty string if in the 192.168.x.x private range",
+    "192.168.1.1",
+    "",
+  ],
+  ["returns empty string outside of the valid range", "1.1.1.256", ""],
+  ["returns the ip if valid", "1.1.1.1", "1.1.1.1"],
+  [
+    "returns the full ip if valid, after ignoring port",
+    "1.1.1.1:443",
+    "1.1.1.1:443",
+  ],
+  [
+    "returns empty string if the ip is a trusted proxy (literal)",
+    "1.1.1.1",
+    "",
+    ["1.1.1.1"],
+  ],
+  [
+    "returns empty string if the ip is a trusted proxy (range)",
+    "1.1.1.1",
+    "",
+    [parseProxy("1.1.1.1/32")],
+  ],
+  [
+    "returns the string if the ip is not a trusted proxy (literal)",
+    "1.1.1.1",
+    "1.1.1.1",
+    ["1.1.1.2"],
+  ],
+  [
+    "returns the string if the ip is not a trusted proxy (range)",
+    "1.1.1.1",
+    "1.1.1.1",
+    [parseProxy("1.1.1.2/32")],
+  ],
+  [
+    "returns the string if the ip is not a trusted proxy (invalid proxy)",
+    "1.1.1.1",
+    "1.1.1.1",
+    [
+      // @ts-expect-error: test how runtime handles non-string proxy.
+      1234,
+    ],
+  ],
+  ["returns empty string if unspecified (ipv6)", "::", ""],
+  ["returns empty string if loopback address", "::1", ""],
+  ["returns empty string if ipv4 mapped address", "::ffff:127.0.0.1", ""],
+  ["returns empty string if ipv4-ipv6 translat range", "64:ff9b:1::", ""],
+  ["returns empty string if discard range", "100::", ""],
+  ["returns empty string if documentation range", "2001:db8::", ""],
+  ["returns empty string if benchmarking range", "2001:2::", ""],
+  ["returns empty string if unique local range", "fc02::", ""],
+  ["returns empty string if unicast link local range", "fe80::", ""],
+  [
+    "returns empty string if the ip address is too short (ipv6)",
+    "ffff:ffff:",
+    "",
+  ],
+  [
+    "returns empty string if the ip address is too long",
+    "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff",
+    "",
+  ],
+  [
+    "returns the ip if it is 'Port Control Protocol Anycast' address",
+    "2001:1::1",
+    "2001:1::1",
+  ],
+  [
+    "returns the ip if it is 'Traversal Using Relays around NAT Anycast' address",
+    "2001:1::2",
+    "2001:1::2",
+  ],
+  ["returns the ip if it is 'AMT' address", "2001:3::", "2001:3::"],
+  [
+    "returns the ip if it is 'AS112-v6' address",
+    "2001:4:112::",
+    "2001:4:112::",
+  ],
+  ["returns the ip if it is 'ORCHIDv2' address", "2001:20::", "2001:20::"],
+  ["returns the ip if valid (ipv6)", "::abcd:c00a:2ff", "::abcd:c00a:2ff"],
+  [
+    "returns the ip if valid, after ignoring scope (ipv6)",
+    "::abcd:c00a:2ff%1",
+    "::abcd:c00a:2ff%1",
+  ],
+  [
+    "returns empty string if the ip is a trusted proxy (ipv6, literal)",
+    "::abcd:c00a:2ff",
+    "",
+    ["::abcd:c00a:2ff"],
+  ],
+  [
+    "returns empty string if the ip is a trusted proxy (ipv6, range)",
+    "::abcd:c00a:2ff",
+    "",
+    [parseProxy("::abcd:c00a:2ff/128")],
+  ],
+  [
+    "returns the string if the ip is not a trusted proxy (ipv6, literal)",
+    "::abcd:c00a:2ff",
+    "::abcd:c00a:2ff",
+    ["::abcd:c00a:2fa"],
+  ],
+  [
+    "returns the string if the ip is not a trusted proxy (ipv6, range)",
+    "::abcd:c00a:2ff",
+    "::abcd:c00a:2ff",
+    [parseProxy("::abcd:c00a:2fa/128")],
+  ],
+  [
+    "returns the string if the ip is not a trusted proxy (ipv6, invalid)",
+    "::abcd:c00a:2ff",
+    "::abcd:c00a:2ff",
+    [
+      // @ts-expect-error: test how runtime handles non-string proxy.
+      1234,
+    ],
+  ],
+];
+
+test("@arcjet/ip", async function (t) {
+  await t.test("should expose the public api", async function () {
+    assert.deepEqual(Object.keys(await import("../index.js")).sort(), [
+      // TODO(@wooorm-arcjet): use named exports.
+      "default",
+      "parseProxy",
+    ]);
+  });
+});
+
+test("`findIp`", async (t) => {
+  await t.test("returns empty string if headers not set", () => {
+    assert.equal(
+      findIp(
+        // @ts-expect-error: test runtime handling of missing headers.
+        {},
+      ),
+      "",
+    );
+  });
+
+  await t.test("returns empty string if headers is null", () => {
+    assert.equal(
+      findIp({
+        // @ts-expect-error: test runtime handling of `null` headers.
+        headers: null,
+      }),
+      "",
+    );
+  });
+
+  await t.test("returns empty string if headers is not object", () => {
+    assert.equal(
+      findIp({
+        // @ts-expect-error: test runtime handling of `""` headers.
+        headers: "",
+      }),
+      "",
+    );
+  });
+
+  await t.test(
+    "supports plain object headers with single value (Node.js `IncomingMessage`)",
+    () => {
+      const request = {
+        headers: {
+          // Node.js lowercases the header keys
+          "x-real-ip": "1.1.1.1",
+        },
+      };
+      assert.equal(findIp(request), "1.1.1.1");
+    },
+  );
+
+  await t.test(
+    "supports plain object headers with array value (Node.js `IncomingMessage`)",
+    () => {
+      const request = {
+        headers: {
+          // Node.js lowercases the header keys
+          "x-forwarded-for": ["1.1.1.1", "2.2.2.2", "3.3.3.3"],
+        },
+      };
+      assert.equal(findIp(request), "3.3.3.3");
+    },
+  );
+
+  await t.test("request: `ip`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp({ headers: new Headers(), ip: input }, { proxies }),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("request: `socket.remoteAddress`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers(), socket: { remoteAddress: input } },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("request: `info.remoteAddress`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers(), info: { remoteAddress: input } },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("request: `requestContext.identity.sourceIp`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            {
+              headers: new Headers(),
+              requestContext: { identity: { sourceIp: input } },
+            },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `X-Client-IP`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["X-Client-IP", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `X-Forwarded-For`", async (t) => {
+    const all: Array<Case> = [
+      ...cases,
+      [
+        "returns the last public IP (ipv4)",
+        "1.1.1.1, 2.2.2.2, 3.3.3.3",
+        "3.3.3.3",
+      ],
+      [
+        "returns the last public IP (ipv6)",
+        "e123::, 3.3.3.3, abcd::",
+        "abcd::",
+      ],
+      [
+        "skips any `unknown` IP (ipv4)",
+        "1.1.1.1, 2.2.2.2, 3.3.3.3, unknown",
+        "3.3.3.3",
+      ],
+      [
+        "skips any `unknown` IP (ipv6)",
+        "e123::, 3.3.3.3, abcd::, unknown",
+        "abcd::",
+      ],
+      [
+        "skips any private IP (ipv4)",
+        "1.1.1.1, 2.2.2.2, 3.3.3.3, 127.0.0.1",
+        "3.3.3.3",
+      ],
+      ["skips any private IP (ipv6)", "e123::, 3.3.3.3, abcd::, ::1", "abcd::"],
+      [
+        "skips any trusted proxy IP (ipv4)",
+        "1.1.1.1, 2.2.2.2, 3.3.3.3",
+        "2.2.2.2",
+        ["3.3.3.3"],
+      ],
+      [
+        "skips any trusted proxy IP (ipv6)",
+        "e123::, 3.3.3.3, abcd::",
+        "3.3.3.3",
+        ["abcd::"],
+      ],
+      [
+        "skips multiple trusted proxy IPs (ipv4)",
+        "1.1.1.1, 2.2.2.2, 3.3.3.3",
+        "1.1.1.1",
+        ["3.3.3.3", "2.2.2.2"],
+      ],
+      [
+        "skips multiple trusted proxy IP (ipv6)",
+        "e123::, 3.3.3.3, abcd::",
+        "e123::",
+        ["3.3.3.3", "abcd::"],
+      ],
+    ];
+
+    for (const [message, input, expected, proxies] of all) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["X-Forwarded-For", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test(
+    "header: `CF-Connecting-IP` (platform: `cloudflare`)",
+    async (t) => {
+      for (const [message, input, expected, proxies] of cases) {
+        await t.test(message, () => {
+          assert.equal(
+            findIp(
+              { headers: new Headers([["CF-Connecting-IP", input]]) },
+              { platform: "cloudflare", proxies },
+            ),
+            expected,
+          );
+        });
+      }
+    },
+  );
+
+  await t.test(
+    "header: `CF-Connecting-IPv6` (platform: `cloudflare`)",
+    async (t) => {
+      for (const [message, input, expected, proxies] of cases) {
+        // `CF-Connecting-IPv6` is only used for IPv6 addresses.
+        if (input.includes(".")) continue;
+
+        await t.test(message, () => {
+          assert.equal(
+            findIp(
+              { headers: new Headers([["CF-Connecting-IPv6", input]]) },
+              { platform: "cloudflare", proxies },
+            ),
+            expected,
+          );
+        });
+      }
+    },
+  );
+
+  await t.test("header: `X-Real-IP` (platform: `vercel`)", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["X-Real-IP", input]]) },
+            { platform: "vercel", proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test(
+    "header: `X-Vercel-Forwarded-For` (platform: `vercel`)",
+    async (t) => {
+      for (const [message, input, expected, proxies] of cases) {
+        await t.test(message, () => {
+          assert.equal(
+            findIp(
+              { headers: new Headers([["X-Vercel-Forwarded-For", input]]) },
+              { platform: "vercel", proxies },
+            ),
+            expected,
+          );
+        });
+      }
+    },
+  );
+
+  await t.test("header: `X-Forwarded-For` (platform: `vercel`)", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["X-Forwarded-For", input]]) },
+            { platform: "vercel", proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `True-Client-IP` (platform: `render`)", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["True-Client-IP", input]]) },
+            { platform: "render", proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `DO-Connecting-IP`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["DO-Connecting-IP", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `Fastly-Client-IP`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["Fastly-Client-IP", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `Fly-Client-IP` (platform: `fly-io`)", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["Fly-Client-IP", input]]) },
+            { platform: "fly-io", proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `True-Client-IP`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["True-Client-IP", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `X-Real-IP`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp({ headers: new Headers([["X-Real-IP", input]]) }, { proxies }),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `X-Cluster-Client-IP`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["X-Cluster-Client-IP", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `X-Forwarded`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["X-Forwarded", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `Forwarded-For`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["Forwarded-For", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `Forwarded`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp({ headers: new Headers([["Forwarded", input]]) }, { proxies }),
+          expected,
+        );
+      });
+    }
+  });
+
+  await t.test("header: `X-Appengine-User-IP`", async (t) => {
+    for (const [message, input, expected, proxies] of cases) {
+      await t.test(message, () => {
+        assert.equal(
+          findIp(
+            { headers: new Headers([["X-Appengine-User-IP", input]]) },
+            { proxies },
+          ),
+          expected,
+        );
+      });
+    }
+  });
+});
