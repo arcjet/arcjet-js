2ee8972bdb0b047280787416f87049954a7cd5a6
Titus Wormer <titus@arcjet.com>
2025-08-08 10:33:57 +0200

    docs(transport): add JSDocs
    Related-to: GH-4741.
    Related-to: GH-3327.

--- transport/index.ts
+++ transport/index.ts
@@ -3,6 +3,16 @@ import {
   Http2SessionManager,
 } from "@connectrpc/connect-node";
 
+/**
+ * Create a transport that talks over HTTP/2 using Connect RPC.
+ *
+ * A thin wrapper around {@linkcode createConnectTransport}.
+ *
+ * @param baseUrl
+ *   Base URI for all HTTP requests (example: `https://example.com/my-api`).
+ * @returns
+ *   Connect transport used to make RPC calls.
+ */
 export function createTransport(baseUrl: string) {
   // We create our own session manager so we can attempt to pre-connect
   const sessionManager = new Http2SessionManager(baseUrl, {
