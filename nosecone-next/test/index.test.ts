de21ae160d2e2c7886be1b87318c3713dc60b72e
Titus Wormer <titus@arcjet.com>
2025-08-11 13:39:58 +0200

    chore(nosecone-next): expose named export, deprecate default
    This commit adds a named export for `nosecone`.
    The default export is still supported but deprecated.
    
    Related-to: GH-4723.
    Related-to: GH-4860.
    Related-to: GH-4875.
    Related-to: GH-4878.

--- nosecone-next/test/index.test.ts
+++ nosecone-next/test/index.test.ts
@@ -5,10 +5,10 @@ test("@nosecone/next", async function (t) {
   await t.test("should expose the public api", async function () {
     assert.deepEqual(Object.keys(await import("../index.js")).sort(), [
       "createMiddleware",
-      // TODO(@wooorm-arcjet): use named exports.
       "default",
       // TODO(@wooorm-arcjet): use a clearer name: defaults for what, function to generate them?
       "defaults",
+      "nosecone",
       "withVercelToolbar",
     ]);
   });
