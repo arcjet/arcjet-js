173520bd8141350a42ecc918c02083f161db5b5c
Titus Wormer <titus@arcjet.com>
2025-08-11 13:26:05 +0200

    chore(ip): expose named export, deprecate default
    This commit adds a named export for `findIp`.
    The default export is still supported but deprecated.
    
    Related-to: GH-4723.
    Related-to: GH-4860.
    Related-to: GH-4875.

--- ip/test/ip.test.ts
+++ ip/test/ip.test.ts
@@ -185,8 +185,8 @@ const cases: Array<Case> = [
 test("@arcjet/ip", async function (t) {
   await t.test("should expose the public api", async function () {
     assert.deepEqual(Object.keys(await import("../index.js")).sort(), [
-      // TODO(@wooorm-arcjet): use named exports.
       "default",
+      "findIp",
       "parseProxy",
     ]);
   });
