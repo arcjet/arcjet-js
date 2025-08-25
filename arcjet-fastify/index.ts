4932cfed81fe8e7228d549dbe61a6c7fbc511221
Titus Wormer <titus@arcjet.com>
2025-08-25 10:50:15 +0200

    blank line

--- arcjet-fastify/index.ts
+++ arcjet-fastify/index.ts
@@ -313,6 +313,7 @@ function toArcjetRequest<Properties extends PlainObject>(
       );
     }
   }
+
   const method = request.method ?? "";
   const host = headers.get("host") ?? "";
   let path = "";
