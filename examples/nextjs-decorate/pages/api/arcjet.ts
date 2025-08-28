557aa67f686728063af255c562bcf3c6c71b41d6
Titus Wormer <titus@arcjet.com>
2025-08-28 15:34:40 +0200

    refactor(examples): move `characteristics` to rules where possible

--- examples/nextjs-decorate/pages/api/arcjet.ts
+++ examples/nextjs-decorate/pages/api/arcjet.ts
@@ -7,12 +7,12 @@ const aj = arcjet({
   // and set it as an environment variable rather than hard coding.
   // See: https://nextjs.org/docs/app/building-your-application/configuring/environment-variables
   key: process.env.ARCJET_KEY!,
-  // Limiting by ip.src is the default if not specified
-  //characteristics: ["ip.src"],
   rules: [
     // Fixed window rate limit. Arcjet also supports sliding window and token
     // bucket.
     fixedWindow({
+      // Limiting by `ip.src` is the default if not specified
+      // characteristics: ["ip.src"],
       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
       window: "1m", // 1 min fixed window
       max: 1, // allow a single request (for demo purposes)
