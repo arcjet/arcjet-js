783db87ac4cb5f76d33531fac96f5cab8aaab8c7
Titus <titus@arcjet.com>
2025-09-09 17:45:55 +0200

    fix(examples): add styles to `remix-express` (#5115)
    * add styles
    * use textarea instead of input, it’s not for just an IP address
    * use a more wordy example instead of just an IP address
    * use `name="text"` instead of `name="email"` so 1password and the like don’t attempt to autofill or store
    * also attempted to update other dependencies, which didn’t work, but I made sure everything that’s possible is up to date, and left some notes
    
    Closes GH-5114.
    
    Before:
    
    <img width="1608" height="1051" alt="Screenshot 2025-09-05 at 4 08 41 PM" src="https://github.com/user-attachments/assets/9c224c4a-44e1-4b85-bb38-4e6f2da656db" />
    
    After:
    
    <img width="1608" height="1051" alt="Screenshot 2025-09-05 at 4 08 21 PM" src="https://github.com/user-attachments/assets/b16308c6-502f-4884-8511-81a1a871e4ab" />

--- examples/remix-express/app/routes/_index.tsx
+++ examples/remix-express/app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { Form, redirect, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
 
 import { aj } from "../arcjet";
 
@@ -18,7 +18,7 @@ export async function loader(args: LoaderFunctionArgs) {
     throw new Response(null, { status: 429, statusText: "Too Many Requests" })
   }
 
-  return json({ message: "Hello Arcjet" })
+  return json({ message: "Hello Arcjet" });
 }
 
 export async function action(args: ActionFunctionArgs) {
@@ -28,23 +28,53 @@ export async function action(args: ActionFunctionArgs) {
     throw new Response(null, { status: 400, statusText: "Form contains sensitive info" })
   }
 
-  return redirect("/");
+  return json({ message: { positive: "No sensitive info detected."} })
 }
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>()
+  const data = useActionData<typeof action>();
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            {data.message}
-          </h1>
+      <div className="flex flex-col items-left gap-16 max-w-[700px]">
+        <header className="flex flex-col items-left gap-9">
+          <div className="flex flex-col items-left gap-5">
+            <h2 className="leading text-xl font-bold text-black dark:text-white">
+              Arcjet + Remix App
+            </h2>
+            <h1 className="leading text-3xl font-bold text-black dark:text-white">
+              Sensitive info detection example
+            </h1>
+          </div>
+          <div className="flex flex-col items-left gap-5">
+            <p className="text-lg">
+              This form uses Arcjet's sensitive info detection feature which is configured to detect credit card numbers. It can be configured to detect other types of sensitive information and custom patterns.
+            </p>
+            <p className="text-gray-600 dark:text-gray-400">
+              The request is analyzed entirely on your server so no sensitive information is sent to Arcjet.
+            </p>
+          </div>
         </header>
-        <Form method="post">
-          <input type="text" name="email" defaultValue="192.168.1.1" />
-          <div>
-            <button type="submit">Check Sensitive Info</button>
+        <Form method="post" className="flex flex-col items-left gap-3">
+          <textarea
+            cols={90}
+            className="block p-3 rounded-xl border focus:outline-none border-gray-300 focus:border-gray-500 focus:outline-none dark:border-gray-800 max-w-full box-border transition-colors ease-in-out"
+            defaultValue="Some text 192.168.1.1 and an email@example.com."
+            name="text"
+            rows={3}
+          />
+          <div className="flex flex-row items-left gap-6 items-center justify-between">
+            <button
+              className="cursor-pointer rounded-3xl font-bold bg-black text-white hover:bg-gray-700 active:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-400 dark:active:bg-gray-300 py-2 px-4 my-2 transition-colors ease-in-out"
+              type="submit"
+            >
+              Check Sensitive Info
+            </button>
+            {data?.message.positive && (
+              <p>
+                No sensitive info detected.
+              </p>
+            )}
           </div>
         </Form>
       </div>
