13c90d6340747f7e5f94768c93b75e353aadac9a
Titus <titus@arcjet.com>
2025-09-16 10:31:03 +0200

    feat(stable-hash, transport): support `workerd` (#5077)
    Related-to: GH-759.
    Closes GH-5076.

--- transport/workerd.ts
+++ transport/workerd.ts
@@ -1,6 +1,6 @@
-// This file is used when running on the `edge-light` condition.
-// Specifically Edge by Vercel.
-// It is the same as `workerd.ts`, which runs on Cloudflare.
+// This file is used when running on the `workerd`.
+// Specifically workers on Cloudflare.
+// It is the same as `edge-light.ts`, which runs on Vercel.
 // It uses DOM based APIs (`@connectrpc/connect-web`) to connect to the API.
 // Differing from `bun.ts` this solves the `redirect` option set to `error`
 // inside `connect` as that does not work on the edge.
