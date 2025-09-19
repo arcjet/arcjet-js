4dee1e74c691583bf58613b8a39283adf5058253
Titus Wormer <titus@arcjet.com>
2025-09-19 11:26:38 +0200

    generate in `analyze-wasm` too

--- analyze-wasm/test/index.test.ts
+++ analyze-wasm/test/index.test.ts
@@ -1,10 +1,85 @@
 import assert from "node:assert/strict";
 import test from "node:test";
+import { initializeWasm } from "../index.js";
+
+const wasm = await initializeWasm({
+  "arcjet:js-req/bot-identifier": {
+    detect() {
+      return [];
+    },
+  },
+  "arcjet:js-req/email-validator-overrides": {
+    hasGravatar() {
+      return "unknown";
+    },
+    hasMxRecords() {
+      return "unknown";
+    },
+    isDisposableEmail() {
+      return "unknown";
+    },
+    isFreeEmail() {
+      return "unknown";
+    },
+  },
+  "arcjet:js-req/filter-overrides": {
+    ipLookup() {
+      return undefined;
+    },
+  },
+  "arcjet:js-req/sensitive-information-identifier": {
+    detect() {
+      return [];
+    },
+  },
+  "arcjet:js-req/verify-bot": {
+    verify() {
+      return "unverifiable";
+    },
+  },
+});
+assert.ok(wasm);
 
 test("@arcjet/analyze-wasm", async function (t) {
   await t.test("should expose the public api", async function () {
     assert.deepEqual(Object.keys(await import("../index.js")).sort(), [
       "initializeWasm",
     ]);
+  });
+});
+
+test("`detectSensitiveInfo`", async function (t) {
+  await t.test("should detect a card number", async function () {
+    assert.deepEqual(
+      wasm.detectSensitiveInfo("a 4242424242424242 b", {
+        entities: { tag: "allow", val: [] },
+        skipCustomDetect: false,
+      }),
+      {
+        allowed: [],
+        denied: [
+          { end: 18, identifiedType: { tag: "credit-card-number" }, start: 2 },
+        ],
+      },
+    );
+  });
+
+  await t.test("should detect a card number if in `allow`", async function () {
+    assert.deepEqual(
+      wasm.detectSensitiveInfo("a 4242424242424242 b", {
+        entities: { tag: "allow", val: [{ tag: "credit-card-number" }] },
+        skipCustomDetect: false,
+      }),
+      {
+        allowed: [
+          {
+            end: 18,
+            identifiedType: { tag: "credit-card-number" },
+            start: 2,
+          },
+        ],
+        denied: [],
+      },
+    );
   });
 });
