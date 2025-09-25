f775f5a9858763c915d28f39a0cc830017d11998
Titus Wormer <titus@arcjet.com>
2025-09-25 14:07:11 +0200

    chore(examples): remove examples showing 3rd parties
    Closes GH-5220.
    Closes GH-5110.

--- examples/nestjs-graphql/src/recipes/recipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RecipesResolver } from './recipes.resolver.js';
-import { RecipesService } from './recipes.service.js';
-
-@Module({
-  providers: [RecipesResolver, RecipesService],
-})
-export class RecipesModule {}
--- examples/nextjs-better-auth/environment.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    readonly ARCJET_KEY: string;
-    readonly GITHUB_CLIENT_ID: string;
-    readonly GITHUB_CLIENT_SECRET: string;
-  }
-}
