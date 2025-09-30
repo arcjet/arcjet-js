149439bebb72a5fc14bfa7876d46b0c360912dcc
Titus Wormer <titus@arcjet.com>
2025-09-30 10:05:16 -0400

    wip: add `@arcjet/nuxt`
    This commit adds the basic files needed for `@arcjet/nuxt`.

---
+++ arcjet-nuxt/test/index.test.ts
@@ -0,0 +1,8 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+
+test("@arcjet/nuxt (api)", async function (t) {
+  await t.test("should expose the public api", async function () {
+    assert.deepEqual(Object.keys(await import("../index.js")).sort(), []);
+  });
+});
