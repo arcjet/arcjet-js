946c227c172be412852c2188b99d483118873e68
blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>
2025-10-02 10:06:26 -0700

    deps(examples): Update `ai` and `@ai-sdk/openai` (#5262)
    Resolving https://github.com/arcjet/arcjet-js/security/dependabot/536 which required updating various direct and transitivie dependencies.

--- examples/nextjs-openai/app/api/chat/route.ts
+++ examples/nextjs-openai/app/api/chat/route.ts
@@ -82,5 +82,5 @@ export async function POST(req: Request) {
     messages,
   });
 
-  return result.toDataStreamResponse();
+  return result.toUIMessageStreamResponse();
 }
--- examples/nextjs-openai/app/page.tsx
+++ examples/nextjs-openai/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useChat } from "ai/react";
+import { useChat } from "@ai-sdk/react";
+import { useState } from 'react';
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const [input, setInput] = useState('');
+  const { messages, sendMessage} = useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((m) => (
@@ -17,16 +19,25 @@ export default function Chat() {
               🤖 <strong>AI:</strong>{" "}
             </>
           )}
-          {m.content}
+          {m.parts.map((part, i) => {
+            switch (part.type) {
+              case 'text':
+                return <div key={`${m.id}-${i}`}>{part.text}</div>;
+            }
+          })}
         </div>
       ))}
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={e => {
+          e.preventDefault();
+          sendMessage({ text: input });
+          setInput('');
+        }}>
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl dark: border-gray-700 dark:bg-gray-800 dark:text-gray-100"
           value={input}
           placeholder="Say something..."
-          onChange={handleInputChange}
+          onChange={e => setInput(e.currentTarget.value)}
         />
       </form>
     </div>
