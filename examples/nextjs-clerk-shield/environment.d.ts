66c71a329275982be70e5a83aae013b76240ddd3
Luigi Morel <morelmiles@gmail.com>
2025-10-03 13:15:29 +0300

    fix: local package links

--- examples/nextjs-clerk-shield/environment.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    readonly ARCJET_KEY: string;
-    readonly NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
-    readonly CLERK_SECRET_KEY: string;
-  }
-}
