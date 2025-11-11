b3bd3b66409812fb835baeff4d7eea2247f70225
Titus Wormer <titus@arcjet.com>
2025-11-11 16:24:05 +0100

    feat(proxies): add `cloudflare` and `google` IP ranges
    Closes GH-5381.

---
+++ proxies/eslint.config.js
@@ -0,0 +1,12 @@
+/**
+ * @import {Linter} from "eslint"
+ */
+
+import base from "@arcjet/eslint-config";
+
+/**
+ * ESLint configuration for this project.
+ *
+ * @type {Array<Linter.Config>}
+ */
+export default [...base, { ignores: ["test/*.js", "index.js"] }];
