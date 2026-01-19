1423f4fb77376228d4c71b430276e349b0995009
Titus <titus@arcjet.com>
2026-01-19 05:28:18 -0500

    chore(examples): remove 3rd party examples (#5633)
    This removes some examples, referencing https://github.com/arcjet/arcjet-js/issues/5220#issuecomment-3715564430.
    See each commit message for more info.
    
    Related-to: GH-5220.

--- examples/nextjs-authjs-5/environment.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-    export interface ProcessEnv {
-      readonly ARCJET_KEY: string;
-      readonly AUTH_SECRET: string;
-      readonly AUTH_GITHUB_ID: string;
-      readonly AUTH_GITHUB_SECRET: string;
-    }
-  }
