790864cf8af9d2e99a75869c857d6c1c60b48dfa
Titus Wormer <titus@arcjet.com>
2026-02-04 12:47:03 +0100

    test(arcjet): add `protectSignup` tests
    Related-to: GH-5772.

--- arcjet-deno/test/index.test.ts
+++ arcjet-deno/test/index.test.ts
@@ -33,6 +33,7 @@ import arcjetDeno, {
   createRemoteClient,
   detectBot,
   filter,
+  protectSignup,
   sensitiveInfo,
   validateEmail,
 } from "../index.js";
@@ -1035,6 +1036,60 @@ test("`arcjetDeno`", async function (t) {
 
     assert.equal(responseInvalid.status, 403);
     assert.equal(responseValid.status, 200);
+  });
+
+  await t.test("should support `protectSignup`", async function () {
+    const restore = capture();
+    let email = "alice";
+
+    const arcjet = arcjetDeno({
+      characteristics: ['http.request.headers["user-agent"]', "ip.src"],
+      client: createLocalClient(),
+      key: exampleKey,
+      rules: [
+        protectSignup({
+          bots: { allow: [], mode: "LIVE" },
+          email: { allow: [], mode: "LIVE" },
+          rateLimit: { interval: 60, max: 5, mode: "LIVE" },
+        }),
+      ],
+    });
+
+    const { server, url } = createSimpleServer({
+      async decide(request) {
+        return arcjet.protect(request, { email });
+      },
+      handler: arcjet.handler,
+    });
+
+    const responseEmailInvalid = await fetch(url);
+
+    email = "alice@arcjet.com";
+
+    const responseEmailValid = await fetch(url);
+
+    const responseUserAgentBot = await fetch(url, {
+      headers: {
+        "user-agent": "Googlebot/2.1 (+http://www.google.com/bot.html)",
+      },
+    });
+
+    const responseUserAgentBrowser = await fetch(url, {
+      headers: {
+        "user-agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+      },
+    });
+
+    // No tests for rate limiting as that happens remotely.
+
+    await server.shutdown();
+    restore();
+
+    assert.equal(responseEmailInvalid.status, 403);
+    assert.equal(responseEmailValid.status, 200);
+    assert.equal(responseUserAgentBot.status, 403);
+    assert.equal(responseUserAgentBrowser.status, 200);
   });
 
   await t.test("should support a custom rule", async function () {
